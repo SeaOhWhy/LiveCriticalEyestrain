c0ca72065326fc149d93b3337e308b5783b0f482
SeaOhWhy <shahara.coy@patelhs.org>
2020-10-09 14:43:17 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args)
+   {
+     int x = 22
+
+     while(x == 22){
+       //if x is equal to 22 then 
+      System.out.println(" x is equal to 22");
+      int++;
+     }
+     System.out.println("x is equal to 22");
+  }
+}
