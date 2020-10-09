59d2b49b69582b103d4f9a59abd90c23d771a56b
SeaOhWhy <shahara.coy@patelhs.org>
2020-10-09 14:59:39 +0000

    I made while loop that says that" x is equal to 22".

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 class Main {
   public static void main(String[] args)
    {
-     int x = 22
+     int x = 22;
 
      while(x == 22){
        //if x is equal to 22 then 
       System.out.println(" x is equal to 22");
-      int++;
+      x++;
+
      }
-     System.out.println("x is equal to 22");
   }
 }
