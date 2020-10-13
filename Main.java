9d999f5ea1cb3efab63590656539ae0afef18523
SeaOhWhy <shahara.coy@patelhs.org>
2020-10-13 12:54:58 +0000

    I Got rid of the code that was in front of the "while" because it didn't need a code in front.

--- Main.java
+++ Main.java
@@ -4,9 +4,12 @@ class Main {
      int y = 16;
      if (y < x )
      //if condition is true then
-     System.out.println("y is less than x);
+     System.out.println("y is less than x");
 
-     }else{
-       ()
-     //if condition is false then
-   System.out.println("y is less than or equal to x");
+     else
+
+    // if condition is false then
+    System.out.println("x is greater than y");
+
+  }
+}
